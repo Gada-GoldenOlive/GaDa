10fd7abb72daa0c0f3cbfd9dd2de9c4ff56ef799
grace915 <grace0807@hanyang.ac.kr>
2022-09-28 20:59:23 +0900

    시간, 거리 function 추가

--- GaDa/src/function/index.js
+++ GaDa/src/function/index.js
@@ -73,3 +73,26 @@ export const getPWIsNotValid = ({pw}) => {
   const result = korean.test(pw);
   return result
 }
+
+export const getKm = ({distance}) => {
+  return distance / 1000;
+};
+
+export const getHour = ({time}) => {
+  return time / 60
+
+}
+
+export const getDistanceFromLatLonInKm = ({lat1,lng1,lat2,lng2}) =>  {
+  const deg2rad = deg => {
+      return deg * (Math.PI/180)
+  }
+
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2-lat1);  // deg2rad below
+  const dLon = deg2rad(lng2-lng1);
+  const a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  const d = R * c; // Distance in km
+  return d;
+}
