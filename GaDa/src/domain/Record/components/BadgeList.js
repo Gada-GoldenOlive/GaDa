ff32e4083e5fcab7546e0057576180d8c771a713
grace915 <grace0807@hanyang.ac.kr>
2022-09-29 16:54:45 +0900

    badge title로 받기

--- GaDa/src/domain/Record/components/BadgeList.js
+++ GaDa/src/domain/Record/components/BadgeList.js
@@ -18,92 +18,100 @@ const BadgeList = () => {
       category: 'WALKWAY',
       code: 3,
       isValid: true,
+      title: '산책로 3개 달성!'
     },
     {
       category: 'REVIEW',
       code: 3,
       isValid: true,
+      title: '리뷰 3개 달성!'
     },
     {
       category: 'WALKTIME',
       code: 3,
       isValid: true,
+      title: '산책 3시간 달성!'
     },
     {
       category: 'DISTANCE',
       code: 3,
       isValid: true,
+      title: '거리 3km 달성!'
     },
     {
       category: 'PIN',
       code: 3,
       isValid: true,
+      title: '핀작성 3개 달성!'
     },
     {
       category: 'FRIEND',
       code: 3,
       isValid: true,
+      title: '친구 3명 달성!'
     },
     {
       category: 'COMMENT',
       code: 3,
       isValid: true,
+      title: '핀댓글 3개 달성!'
     },
     {
       category: 'WALKWAY',
       code: 5,
       isValid: false,
+      title: '산책로 5개 달성!'
     },
     {
       category: 'REVIEW',
       code: 5,
       isValid: false,
+      title: '리뷰 5개 달성!'
     },
     {
       category: 'WALKTIME',
       code: 5,
       isValid: false,
+      title: '산책 5시간 달성!'
     },
     {
       category: 'DISTANCE',
       code: 5,
       isValid: false,
+      title: '거리 5km 달성!'
     },
     {
       category: 'PIN',
       code: 5,
       isValid: false,
+      title: '핀작성 5개 달성!'
     },
     {
       category: 'FRIEND',
       code: 5,
       isValid: false,
+      title: '친구 5명 달성!'
     },
     {
       category: 'COMMENT',
       code: 5,
       isValid: false,
+      title: '핀댓글 5개 달성!'
     },
   ];
-  const getTitle = item => {
-    const { category, code } = item;
-    switch (category) {
-      case 'WALKWAY':
-        return `산책로 ${code}개 달성!`;
-      case 'REVIEW':
-        return `리뷰 ${code}개 달성!`;
-      case 'WALKTIME':
-        return `산책 ${code}시간 달성!`;
-      case 'DISTANCE':
-        return `거리 ${code}km 달성!`;
-      case 'PIN':
-        return `핀 ${code}개 달성!`;
-      case 'FRIEND':
-        return `친구 ${code}명 달성!`;
-      case 'COMMENT':
-        return `댓글 ${code}개 달성!`;
-    }
-  };
+  
+
+  /**
+ * 피그마에 올라와 있는 순서대로 적어둠 (괄호 안은 code)
+ * WALKWAY = 산책로 n개 달성! (THREE~HUNDRED) / 산책로 등록! (FIRST)
+ * REVIEW = 리뷰 n개 달성! (THREE~HUNDRED)
+ * WALKTIME = 산책 n시간 달성! (THREE~FIFTY) / 목표 시간 설정! (FIRST)
+ * DISTANCE = 거리 n시간 달성! (THREE~FIFTY)
+ * PIN = 핀작성 n개 달성! (THREE~HUNDRED)
+ * FRIEND = 친구 n명 달성! (THREE~FIFTY) / 처음 친구 초대! (FIRST) / 친구를 이겼어요! (WIN) / 1위 달성! (BEST)
+ * COMMENT = 댓글 n개 달성! (THREE~HUNDRED)
+ * USER = 회원가입 완료! (FIRST)
+ */
   const getNum = code => {
     switch (code) {
       case 'THREE':
@@ -144,7 +152,7 @@ const BadgeList = () => {
     }
   };
   const renderItem = ({ item, index }) => {
-    const { category, code, isValid } = item;
+    const {title, category, code, isValid } = item;
 
     return (
       <View
@@ -154,7 +162,7 @@ const BadgeList = () => {
         ]}
       >
         <CustomImage style={styles.image} source={getImage(item)} />
-        <Text style={styles.text}>{getTitle(item)}</Text>
+        <Text style={styles.text}>{title}</Text>
       </View>
     );
   };
