f0f78c0fad2023a75ae89c7eaa920c1522bec228
grace915 <grace0807@hanyang.ac.kr>
2022-10-09 21:49:59 +0900

    presigned 어케하는데

--- GaDa/src/APIs/review/index.js
+++ GaDa/src/APIs/review/index.js
@@ -69,3 +69,4 @@ export const getDetailFeed = async reviewId => {
     .catch(handleNetworkError);
   return res;
 };
+
--- GaDa/src/components/ImageSubmitButton.js
+++ GaDa/src/components/ImageSubmitButton.js
@@ -8,9 +8,14 @@ import { blackColor, emphasisColorVer2 } from '../constant/colors';
 import { useDispatch, useSelector } from 'react-redux';
 import { setPinImage, setUploadImagesChanged } from '../redux/modules/images';
 import { createNewMessage } from '../APIs/Chat';
+import { getPreSignedUrl, uploadImage } from '../APIs/image';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const HeaderImageSubmitButton = props => {
   const { imageList, ver, body } = props;
+  const [eachUrl, setEachUrl] = useState('');
+
   const navigation = useNavigation();
 
   const dispatch = useDispatch();
@@ -18,21 +23,35 @@ const HeaderImageSubmitButton = props => {
     bodyPhotoCount: images.bodyPhotoCount,
     bodyPhotoImages: images.bodyPhotoImages,
   }));
-
-  const handleClick = () => {
+  const getURL = async () => {
+    const res = await getPreSignedUrl();
+    if(res){
+      const {url} = res;
+      setEachUrl(url);
+    }
+  }
+  const handleClick = async () => {
     if (ver === 'pin') {
       // pin
       imageList.forEach(async item => {
+
         const uri = `data:${item.imageData.mime};base64,${item.imageData.data}`;
+        await uploadImage(eachUrl, uri);
+        
         const setImages = items => {
           dispatch(setPinImage(items));
           dispatch(setUploadImagesChanged(true));
         };
-        setImages(uri);
+        
+        setImages(eachUrl);
       });
       navigation.pop();
     }
   };
+
+  useEffect(() => {
+    getURL();
+  }, [])
   return (
     <TouchableWithoutFeedback onPress={handleClick}>
       <View style={styles.textButton} {...props}>
